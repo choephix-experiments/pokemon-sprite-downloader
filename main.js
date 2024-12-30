d633f3885920fc9e6f64fe365c1eda500f3a2b4b
choephix <choephix@gmail.com>
2024-12-30 21:23:56 +0200

    Implement concurrent image downloads for Pokémon artwork and save metadata to JSON

--- main.js
+++ main.js
@@ -3,29 +3,52 @@ const fs = require('fs');
 const path = require('path');
 
 const downloadDir = './downloads/pokemon-official-artwork';
+const concurrency = 6;
 
 // Ensure the download directory exists
 if (!fs.existsSync(downloadDir)) {
   fs.mkdirSync(downloadDir, { recursive: true });
 }
 
-async function main() {
-  for (const pokemon of all.results) {
-    const res = await fetch(pokemon.url);
-    const json = await res.json();
-    const imageUrl = json.sprites.other['official-artwork'].front_default;
-    const id = json.id;
+const pokemonJsons = []
 
-    if (imageUrl) {
-      const imageRes = await fetch(imageUrl);
-      const arrayBuffer = await imageRes.arrayBuffer();
-      const buffer = Buffer.from(arrayBuffer);
-      // const fileName = path.join(downloadDir, `${pokemon.name}.png`);
-      const fileName = path.join(downloadDir, `${id}.png`);
-      fs.writeFileSync(fileName, buffer);
-      console.log(`Downloaded ${pokemon.name} to ${fileName}`);
+const downloadImage = async pokemon => {
+  const res = await fetch(pokemon.url);
+  const json = await res.json();
+  const imageUrl = json.sprites.other['official-artwork'].front_default;
+  const id = json.id;
+
+  if (imageUrl) {
+    const imageRes = await fetch(imageUrl);
+    const arrayBuffer = await imageRes.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    const fileName = path.join(downloadDir, `${id}.png`);
+    fs.writeFileSync(fileName, buffer);
+    console.log(`Downloaded ${pokemon.name} to ${fileName}`);
+  }
+
+  pokemonJsons.push(json);
+};
+
+const worker = async queue => {
+  while (queue.length > 0) {
+    const pokemon = queue.pop();
+    if (pokemon) {
+      await downloadImage(pokemon);
     }
   }
-}
+};
+
+const main = async () => {
+  const queue = [...all.results];
+  const workers = Array(concurrency)
+    .fill(null)
+    .map(() => worker(queue));
+  await Promise.all(workers);
+
+  const jsonFilePath = path.join(downloadDir, 'rip.json');
+  fs.writeFileSync(jsonFilePath, JSON.stringify(pokemonJsons, null, 2));
+  console.log(`Saved JSON to ${jsonFilePath}`);
+};
 
 main();
